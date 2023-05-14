36ed35a47f52b5405ac66b27e70f0abe471ad083
VisnuPKumarr <85438944+VisnuPKumarr@users.noreply.github.com>
2023-05-14 21:56:50 +0530

    Add files via upload

---
+++ movie-catalog-service/src/main/java/com/example/demo/services/Movieinfo.java
@@ -0,0 +1,31 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.models.CatalogItem;
+import com.example.demo.models.Movie;
+import com.example.demo.models.Rating;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Service
+public class Movieinfo {
+
+	public Movieinfo() {
+		// TODO Auto-generated constructor stub
+	}
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+	public CatalogItem getCatalogItem(Rating rating) {
+		Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+		return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+	}
+	
+	public CatalogItem getFallbackCatalogItem(Rating rating) {
+		return new CatalogItem("Movie name not found", "", rating.getRating());
+	}
+
+}
